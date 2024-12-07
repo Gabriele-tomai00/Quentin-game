cddf37cb27fe4272e47c13735e5883c09c503004
Gabriele <tomaigabriele@gmail.com>
2024-12-07 21:10:35 +0100

    add Board and test (BoardTest and HasWonTest)

---
+++ src/main/java/quentin/Board.java
@@ -0,0 +1,165 @@
+package quentin;
+
+import java.util.Arrays;
+
+public class Board {
+
+  private final int SIZE = 13; // Board size (specified in rules)
+  private final BoardPoint[][] board = new BoardPoint[SIZE][SIZE];
+  private final boolean[][] visited = new boolean[SIZE][SIZE];
+
+  public Board() {
+    for (BoardPoint[] row : board) {
+      Arrays.fill(row, BoardPoint.EMPTY);
+    }
+  }
+
+  // second constructor for testing
+  public Board(String[][] initialBoard) {
+    if (initialBoard.length != SIZE || initialBoard[0].length != SIZE) {
+      throw new IllegalArgumentException(
+          "Matrix size must be: "
+              + SIZE
+              + "x"
+              + SIZE
+              + ". Your matrix is "
+              + initialBoard.length
+              + "x"
+              + initialBoard[0].length);
+    }
+
+    // this.board = new BoardPoint[SIZE][SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        visited[i][j] = false;
+        String cell = initialBoard[i][j];
+        if (!cell.equals(".") && !cell.equals("B") && !cell.equals("W")) {
+          throw new IllegalArgumentException(
+              "Not valid value in matrix: ("
+                  + cell
+                  + " in "
+                  + j
+                  + ","
+                  + i
+                  + " position). The only allowed values are '.', 'B' and 'W'");
+        }
+        this.board[i][j] = BoardPoint.fromString(cell);
+      }
+    }
+  }
+
+  public BoardPoint[][] getBoard() {
+    return board;
+  }
+
+  public BoardPoint getValues(int i, int j) {
+    if (i < SIZE && j < SIZE) return board[i][j];
+    else throw new RuntimeException("Coordinates not valid");
+  }
+
+  public boolean isMoveValid(Player player, int row, int col, boolean isFirstMove) {
+    // check if the cell is valid: inside the board
+    if (row >= 0 && row < SIZE && col >= 0 && col < SIZE) {
+      if (board[row][col] == BoardPoint.WHITE || board[row][col] == BoardPoint.BLACK) return false;
+
+      if (!isFirstMove) {
+        // check if the stone is orthogonally close another stone with the same color
+        try {
+          if (board[row - 1][col] == player.color()) return true;
+        } catch (ArrayIndexOutOfBoundsException e) { // Ignore exception and continue execution
+        }
+        try {
+          if (board[row][col - 1] == player.color()) return true;
+        } catch (ArrayIndexOutOfBoundsException e) { // Ignore exception and continue execution
+        }
+        try {
+          if (board[row + 1][col] == player.color()) return true;
+        } catch (ArrayIndexOutOfBoundsException e) { // Ignore exception and continue execution
+        }
+        try {
+          if (board[row][col + 1] == player.color()) return true;
+        } catch (ArrayIndexOutOfBoundsException e) { // Ignore exception and continue execution
+        }
+      } else return true;
+    }
+
+    return false;
+  }
+
+  public void placeStone(BoardPoint stone, int row, int col) {
+    board[row][col] = stone;
+  }
+
+  public boolean hasWon(BoardPoint color) {
+    for (boolean[] row : visited) {
+      Arrays.fill(row, false);
+    }
+    if (color == BoardPoint.WHITE) {
+      for (int i = 0; i < SIZE; i++) if (findWinnerPath(BoardPoint.WHITE, i, 0)) return true;
+    } else if (color == BoardPoint.BLACK) {
+      for (int j = 0; j < SIZE; j++) if (findWinnerPath(BoardPoint.BLACK, 0, j)) return true;
+    } else throw new RuntimeException("Not valid color");
+    return false;
+  }
+
+  public boolean findWinnerPath(BoardPoint color, int i, int j) {
+    if (visited[i][j] || board[i][j] != color) {
+      return false;
+    }
+    visited[i][j] = true;
+
+    // orthogonal direction
+    // right
+    if (j + 1 < SIZE && findWinnerPath(color, i, j + 1)) {
+      return true;
+    }
+    // left
+    if (j - 1 >= 0 && findWinnerPath(color, i, j - 1)) {
+      return true;
+    }
+    // up
+    if (i - 1 >= 0 && findWinnerPath(color, i - 1, j)) {
+      return true;
+    }
+    // down
+    if (i + 1 < SIZE && findWinnerPath(color, i + 1, j)) {
+      return true;
+    }
+
+    return color == BoardPoint.WHITE ? j == SIZE - 1 : i == SIZE - 1;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder toReturn = new StringBuilder();
+    toReturn.append("          0     1    2    3   4     5    6    7    8    9    10  11  12\n");
+    toReturn.append("             B     B    B    B   B     B    B    B    B    B    B   B\n");
+    toReturn.append(
+        "     W  ┌────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┐ W\n");
+
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        if (j == 0) {
+          if (i > 9) toReturn.append(i).append("      ");
+          else toReturn.append(i).append("       ");
+        }
+        toReturn.append("│");
+        if (board[i][j].equals(BoardPoint.EMPTY)) toReturn.append("    ");
+        else toReturn.append(" ").append(board[i][j]).append("  ");
+
+        if (j == SIZE - 1) toReturn.append("│");
+      }
+      toReturn.append("\n");
+      if (i == SIZE - 1) {
+        toReturn.append(
+            "     W  └────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┘ W\n");
+      } else {
+        toReturn.append(
+            "     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W\n");
+      }
+    }
+    toReturn.append("          B     B    B    B   B     B    B    B    B    B    B   B     B\n");
+
+    return toReturn.toString();
+  }
+}
---
+++ src/test/java/quentin/BoardTest.java
@@ -0,0 +1,274 @@
+package quentin;
+
+import static org.junit.jupiter.api.Assertions.*; // JUnit 5
+
+import org.junit.jupiter.api.Test; // Test of JUnit 5
+
+public class BoardTest {
+
+  public static boolean areMatricesEqual(String[][] matrix1, String[][] matrix2) {
+    if (matrix1.length != matrix2.length || matrix1[0].length != matrix2[0].length) {
+      return false;
+    }
+    for (int i = 0; i < matrix1.length; i++) {
+      for (int j = 0; j < matrix1[i].length; j++) {
+        if (!matrix1[i][j].equals(matrix2[i][j])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  @Test
+  public void testValidMatrixSize() {
+    String[][] array = {
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."}
+    };
+
+    assertDoesNotThrow(
+        () -> {
+          new Board(array);
+        });
+  }
+
+  @Test
+  public void testInvalidMatrixSize() {
+    String[][] invalidBoard = {
+      {".", ".", ".", ".", "."},
+      {".", "W", ".", ".", "."},
+      {".", ".", "B", ".", "."},
+      {".", ".", ".", ".", "."},
+      {".", ".", ".", ".", "."},
+    };
+
+    Exception exception =
+        assertThrows(IllegalArgumentException.class, () -> new Board(invalidBoard));
+    assertEquals("Matrix size must be: 13x13. Your matrix is 5x5", exception.getMessage());
+  }
+
+  @Test
+  public void testCorrectValuesInMatrix() {
+    // the only allowed values are "." "B" "W"
+    String[][] array = {
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", "B", "B", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", "W", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", "W", "W", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."}
+    };
+
+    assertDoesNotThrow(
+        () -> {
+          new Board(array);
+        });
+  }
+
+  @Test
+  public void testIncorrectValuesInMatrix() {
+    // the only allowed values are "." "B" "W"
+    String[][] array = {
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", "B", "B", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", "A", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", "W", "C", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", "W", "W", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", null, ".", ".", ".", ".", ".", ".", "M", ".", ".", ".", "."}
+    };
+
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> new Board(array));
+    assertEquals(
+        "Not valid value in matrix: (A in 8,3 position). The only allowed values are '.', 'B' and 'W'",
+        exception.getMessage());
+  }
+
+  @Test
+  public void testSomeBoardFunctionality() {
+    Board board = new Board();
+    String result = board.toString();
+    String expectedOutput =
+        """
+          0     1    2    3   4     5    6    7    8    9    10  11  12
+             B     B    B    B   B     B    B    B    B    B    B   B
+     W  ┌────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┐ W
+0       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+1       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+2       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+3       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+4       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+5       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+6       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+7       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+8       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+9       │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+10      │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+11      │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤ W
+12      │    │    │    │    │    │    │    │    │    │    │    │    │    │
+     W  └────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┘ W
+          B     B    B    B   B     B    B    B    B    B    B   B     B
+""";
+    assertEquals(expectedOutput, result);
+  }
+
+  @Test
+  public void testisMoveFistMoveValid() {
+    Board board = new Board();
+    Player p = new Player(BoardPoint.BLACK);
+    // valid
+    assertTrue(board.isMoveValid(p, 0, 0, true));
+    assertTrue(board.isMoveValid(p, 12, 12, true));
+    assertTrue(board.isMoveValid(p, 6, 6, true));
+
+    // not valid
+    assertFalse(board.isMoveValid(p, -1, 0, true));
+    assertFalse(board.isMoveValid(p, 0, -1, true));
+    assertFalse(board.isMoveValid(p, 13, 0, true));
+    assertFalse(board.isMoveValid(p, 0, 13, true));
+    assertFalse(board.isMoveValid(p, 14, 14, true));
+  }
+
+  @Test
+  public void testisMoveValid() {
+    Board board = new Board();
+    Player p = new Player(BoardPoint.BLACK);
+    // the board is empty, so it's not possibile to put a stone because there are
+    // no other stones, and It's not the first move
+    assertFalse(board.isMoveValid(p, 0, 0, false));
+    assertFalse(board.isMoveValid(p, 12, 12, false));
+    assertFalse(board.isMoveValid(p, 6, 6, false));
+    // now I add a stone with the same color (so the same player) and I try to put
+    // another stone orthogonally close
+    // CORNER 1
+    board.placeStone(BoardPoint.BLACK, 0, 0);
+    assertTrue(board.isMoveValid(p, 0, 1, false));
+    assertTrue(board.isMoveValid(p, 1, 0, false));
+    assertFalse(board.isMoveValid(p, 1, 1, false));
+    assertFalse(board.isMoveValid(p, 0, 0, false)); // same position
+    // test all the matrix!
+    for (int i = 1; i < board.getBoard().length; i++) {
+      for (int j = 1; j < board.getBoard()[1].length; j++) {
+        assertFalse(board.isMoveValid(p, i, j, false));
+      }
+    }
+    // CORNER 2
+    board.placeStone(BoardPoint.BLACK, 12, 0);
+    assertTrue(board.isMoveValid(p, 12, 1, false));
+    assertTrue(board.isMoveValid(p, 11, 0, false));
+    assertFalse(board.isMoveValid(p, 11, 1, false)); // diagonal
+    assertFalse(board.isMoveValid(p, 12, 0, false)); // same position
+    // CORNER 3
+    board.placeStone(BoardPoint.BLACK, 0, 12);
+    assertTrue(board.isMoveValid(p, 0, 11, false));
+    assertTrue(board.isMoveValid(p, 1, 12, false));
+    assertFalse(board.isMoveValid(p, 1, 11, false)); // diagonal
+    // CORNER 4
+    board.placeStone(BoardPoint.BLACK, 12, 12);
+    assertTrue(board.isMoveValid(p, 12, 11, false));
+    assertTrue(board.isMoveValid(p, 11, 12, false));
+    assertFalse(board.isMoveValid(p, 11, 11, false)); // diagonal
+    // IN THE MIDDLE OF THE MATRIX
+    board.placeStone(BoardPoint.BLACK, 6, 6);
+    assertTrue(board.isMoveValid(p, 6, 5, false));
+    assertTrue(board.isMoveValid(p, 6, 7, false));
+    assertTrue(board.isMoveValid(p, 5, 6, false));
+    assertTrue(board.isMoveValid(p, 7, 6, false));
+    assertFalse(board.isMoveValid(p, 7, 7, false)); // diagonal
+    assertFalse(board.isMoveValid(p, 5, 7, false)); // diagonal
+    assertFalse(board.isMoveValid(p, 5, 5, false)); // diagonal
+    assertFalse(board.isMoveValid(p, 7, 5, false)); // diagonal
+  }
+
+  @Test
+  public void testCorrectPlaceStone() {
+    Board board = new Board();
+    Player player1 = new Player(BoardPoint.BLACK);
+    assertSame(BoardPoint.BLACK, player1.color());
+    board.placeStone(BoardPoint.BLACK, 1, 2);
+    String[][] matrix1 = BoardPoint.toMatrixString(board.getBoard());
+    String[][] matrix2 = {
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", "B", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."}
+    };
+    assertTrue(areMatricesEqual(matrix1, matrix2));
+  }
+
+  @Test
+  public void testIncorrectPlaceStone() {
+    Board board = new Board();
+    board.placeStone(BoardPoint.BLACK, 1, 2);
+    String[][] matrix1 = BoardPoint.toMatrixString(board.getBoard());
+    String[][] matrix2 = {
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", "W", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."},
+      {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."}
+    };
+
+    assertSame(
+        matrix1[1][3],
+        matrix2[1][3],
+        matrix1[1][3] + " == " + matrix2[1][3]); // this is always true
+    // because I'm checking the
+    // single element
+    assertFalse(areMatricesEqual(matrix1, matrix2));
+  }
+}
