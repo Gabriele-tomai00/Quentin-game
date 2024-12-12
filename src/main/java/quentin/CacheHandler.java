29dd722e3040a40c74b4bb6c2ede54f9f13aa4d0
Gabriele <tomaigabriele@gmail.com>
2024-12-12 15:48:09 +0100

    big improvement for CacheHandler: moves counter, go back in the game, change the move

--- src/main/java/quentin/CacheHandler.java
+++ src/main/java/quentin/CacheHandler.java
@@ -3,84 +3,100 @@ package quentin;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CacheHandler {
+  // the program uploads and downloads in List logCache during the game.
+  public final List<BoardLog> logsInMemory;
+  // If the program is closed, it uses the cache saved in disk
   private static final String CACHE_DIR = System.getProperty("user.home") + "/.quentinCache";
   private static final String CACHE_FILE = CACHE_DIR + "/quentin_cache.dat";
   private static final DateTimeFormatter TIMESTAMP_FORMATTER =
       DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
   private final BufferedWriter writer;
-  private int
-      logCounter; // It's always updated (you can get a specific log without reading all the cache
-  // file every time)
+  private int moveIndex; // helps to decide when and what logs to delete
 
   // LOG STRUCTURE: yyyyMMdd_HHmmss board next_move
   // example: 20241208_172807 1....B............B............B............B............B W
 
   public CacheHandler() {
+    this.logsInMemory = new ArrayList<>();
+    this.moveIndex = -1;
+
+    // Crea la directory di cache, se non esiste
     File cacheDirectory = new File(CACHE_DIR);
-    if (!cacheDirectory.exists()) {
-      if (!cacheDirectory.mkdirs()) {
-        throw new RuntimeException("Failed to create cache directory: " + CACHE_DIR);
-      }
+    if (!cacheDirectory.exists() && !cacheDirectory.mkdirs()) {
+      throw new RuntimeException("Failed to create cache directory: " + CACHE_DIR);
     }
-    logCounter = calculateMoveCounter(); // used only one
+
     try {
-      // Open the writer only once in the constructor
       this.writer = new BufferedWriter(new FileWriter(CACHE_FILE, true));
     } catch (IOException e) {
       throw new RuntimeException("Error initializing file writer", e);
     }
   }
 
-  public void writeLog(String compactBoard, Player nextPlayer) {
+  public int getMoveIndex() {
+    return moveIndex;
+  }
+
+  public void saveLog(String compactBoard, Player nextPlayer) {
     try {
+      if (moveIndex != logsInMemory.size() - 1) {
+        removeElementsAfterIndex(moveIndex); // REMOVE ALL ELEMENT AFTER INDEX ELEMENT
+      }
+
       String timestamp = LocalDateTime.now().format(TIMESTAMP_FORMATTER);
-      writer.write(timestamp + " " + compactBoard + " " + nextPlayer.color().toString());
+      writer.write(timestamp + " " + compactBoard + " " + nextPlayer.color());
       writer.newLine();
       writer.flush(); // Ensure the log is written immediately
-      logCounter++; // I update the log counter, so I can know the number of moves without reading
-      // all the file every time
+      logsInMemory.add(parseStringLog(timestamp + " " + compactBoard + " " + nextPlayer.color()));
+
     } catch (IOException e) {
       throw new RuntimeException("Error while saving to cache file", e);
     }
+    moveIndex = moveIndex + 1;
+    System.out.println("moveIndex " + moveIndex + ": " + compactBoard);
   }
 
-  // Metodo per leggere il log dato un indice
-  public BoardLog readLog(int index) {
-
-    if (index < 0 || index >= logCounter) {
-      throw new IllegalStateException("Invalid log index");
+  public void removeElementsAfterIndex(int index) {
+    if (index > 0 && index <= logsInMemory.size()) {
+      logsInMemory.subList(index + 1, logsInMemory.size()).clear();
+      emptyCacheFile();
     }
+  }
 
+  public void loadLogsInMemoryFromDisk() {
+    // loads all the cache logs on the memory list
     try (BufferedReader reader = new BufferedReader(new FileReader(CACHE_FILE))) {
       String line;
-      int counter = 0;
       while ((line = reader.readLine()) != null) {
-        if (counter == index) {
-          return parseLogLine(line);
-        }
-        counter++;
+        logsInMemory.add(parseStringLog(line));
       }
     } catch (IOException e) {
       throw new IllegalStateException("Problem with reading log file", e);
     }
-    return null; // Se non trovato, ritorna null
   }
 
-  // Funzione per caricare l'ultimo log
+  public BoardLog readLog(int index) {
+    if (index > logsInMemory.size() || index < 0) {
+      throw new IndexOutOfBoundsException(
+          "Index out of bounds in readLog: index " + index + " not present in log list");
+    }
+    moveIndex = index;
+    return logsInMemory.get(index);
+  }
+
+  public int getLogCounter() {
+    return logsInMemory.size();
+  }
+
   public BoardLog readLastLog() {
-    if (logCounter == 0) {
-      return null; // No logs
-    }
-    try {
-      return readLog(logCounter - 1);
-    } catch (Exception e) {
-      throw new IllegalStateException("Unable to read last log", e);
-    }
+    return readLog(logsInMemory.size() - 1);
   }
 
-  private BoardLog parseLogLine(String logLine) {
+  private BoardLog parseStringLog(String logLine) {
     String[] toReturn = logLine.split(" ");
     if (toReturn.length != 3) {
       throw new IllegalStateException("Log format is incorrect");
@@ -89,38 +105,20 @@ public class CacheHandler {
   }
 
   public void clearCache() {
+    emptyCacheFile();
+    logsInMemory.clear();
+  }
+
+  public void emptyCacheFile() {
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(CACHE_FILE))) {
-      // Svuota il contenuto del file, senza eliminarlo
-      writer.write(""); // Scrive una stringa vuota, svuotando il file
+      writer.write("");
       writer.flush();
-      logCounter = 0;
     } catch (IOException e) {
       throw new RuntimeException("Failed to clear the cache file", e);
     }
   }
 
-  public void closeWriter() {
-    try {
-      if (writer != null) writer.close();
-    } catch (IOException e) {
-      throw new RuntimeException("Error while closing writer", e);
-    }
-  }
-
-  private int calculateMoveCounter() {
-    try (BufferedReader reader = new BufferedReader(new FileReader(CACHE_FILE))) {
-      int counter = 0;
-      while (reader.readLine() != null) {
-        counter++;
-      }
-      return counter;
-    } catch (IOException e) {
-      throw new IllegalStateException(
-          "Unable to read the log file. The number of moves cannot be determined.");
-    }
-  }
-
-  public int getLogCounter() {
-    return logCounter;
+  public int calculateMoveCounter() {
+    return moveIndex + 1;
   }
 }
--- src/test/java/quentin/CacheHandlerTest.java
+++ src/test/java/quentin/CacheHandlerTest.java
@@ -7,62 +7,19 @@ import org.junit.jupiter.api.Test; // Test of JUnit 5
 public class CacheHandlerTest {
 
   @Test
-  public void testWriteLog() {
+  public void testSaveAndReadLog() {
     CacheHandler cache = new CacheHandler();
     Player whitePlayer = new Player(BoardPoint.WHITE);
+    cache.clearCache();
+    cache.saveLog("1....B............B............B............B............B", whitePlayer);
+    cache.saveLog("2....B............B............B............B............B", whitePlayer);
+    cache.saveLog("3....B............B............B............B............B", whitePlayer);
 
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
+    Exception exceptionForNegativeIndex =
+        assertThrows(IndexOutOfBoundsException.class, () -> cache.readLog(-1).board());
     assertEquals(
-        "1....B............B............B............B............B", cache.readLog(0).board());
-    cache.clearCache(); // for the next test
-  }
-
-  @Test
-  public void testGetLogCounter() {
-
-    // calculateMoveCounter() read all the logs in the file and return the result (should use only
-    // once inside the class)
-    // getLogCounter() return the number of logs/moves that is updated every time a log is written
-    // on the file (to use outside the class)
-    CacheHandler cache = new CacheHandler();
-    Player whitePlayer = new Player(BoardPoint.WHITE);
-    cache.clearCache();
-
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
-    cache.writeLog("2....B............B............B............B............B", whitePlayer);
-    cache.writeLog("3....B............B............B............B............B", whitePlayer);
-    assertEquals(3, cache.getLogCounter());
-    cache.clearCache();
-
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
-    cache.writeLog("2....B............B............B............B............B", whitePlayer);
-    cache.clearCache();
-
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
-    cache.writeLog("2....B............B............B............B............B", whitePlayer);
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
-    cache.writeLog("2....B............B............B............B............B", whitePlayer);
-    assertEquals(4, cache.getLogCounter());
-    cache.closeWriter();
-
-    CacheHandler cache1 =
-        new CacheHandler(); // I open again the same cache file and I get the same counter
-    assertEquals(4, cache1.getLogCounter());
-
-    cache.clearCache(); // for the next test
-  }
-
-  @Test
-  public void testWriteLogs() {
-    CacheHandler cache = new CacheHandler();
-    Player whitePlayer = new Player(BoardPoint.WHITE);
-    Player blackPlayer = new Player(BoardPoint.BLACK);
-    cache.clearCache();
-
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
-    cache.writeLog("2....B............B............B............B............B", blackPlayer);
-    cache.writeLog("3....B............B............B............B............B", whitePlayer);
-
+        "Index out of bounds in readLog: index -1 not present in log list",
+        exceptionForNegativeIndex.getMessage());
     assertEquals(
         "1....B............B............B............B............B", cache.readLog(0).board());
     assertEquals(
@@ -70,10 +27,24 @@ public class CacheHandlerTest {
     assertEquals(
         "3....B............B............B............B............B", cache.readLog(2).board());
 
-    assertEquals("W", cache.readLog(0).nextMove());
-    assertEquals("B", cache.readLog(1).nextMove());
-    assertEquals("W", cache.readLog(2).nextMove());
-    cache.clearCache(); // for the next test
+    assertThrows(IndexOutOfBoundsException.class, () -> cache.readLog(3).board());
+
+    cache.clearCache();
+  }
+
+  @Test
+  public void testRealBoard() {
+    CacheHandler cache = new CacheHandler();
+    Player whitePlayer = new Player(BoardPoint.WHITE);
+    cache.clearCache();
+
+    Board board = new Board();
+    cache.saveLog(board.toCompactString(), whitePlayer);
+    assertEquals(
+        ".........................................................................................................................................................................",
+        cache.readLog(0).board());
+
+    cache.clearCache();
   }
 
   @Test
@@ -81,37 +52,113 @@ public class CacheHandlerTest {
     CacheHandler cache = new CacheHandler();
     Player whitePlayer = new Player(BoardPoint.WHITE);
     cache.clearCache();
+    cache.saveLog("1....B............B............B............B............B", whitePlayer);
+    cache.saveLog("2....B............B............B............B............B", whitePlayer);
+    cache.saveLog("3....B............B............B............B............B", whitePlayer);
 
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
-    cache.writeLog("2....B............B............B............B............B", whitePlayer);
-    cache.writeLog("3....B............B............B............B............B", whitePlayer);
+    Exception exceptionForNegativeIndex =
+        assertThrows(IndexOutOfBoundsException.class, () -> cache.readLog(-1).board());
+    assertEquals(
+        "Index out of bounds in readLog: index -1 not present in log list",
+        exceptionForNegativeIndex.getMessage());
 
     assertEquals(
         "3....B............B............B............B............B", cache.readLastLog().board());
-    cache.clearCache(); // for the next test
+    assertEquals("W", cache.readLastLog().nextMove());
+    cache.clearCache();
+    CacheHandler newCache = new CacheHandler();
+    assertThrows(IndexOutOfBoundsException.class, () -> newCache.readLastLog().board());
   }
 
   @Test
-  public void testMainSimulation() {
+  public void testLoadLogFromDisk() {
     CacheHandler cache = new CacheHandler();
     Player whitePlayer = new Player(BoardPoint.WHITE);
+    cache.clearCache();
+    cache.saveLog("1....B............B............B............B............B", whitePlayer);
+    cache.saveLog("2....B............B............B............B............B", whitePlayer);
+    cache.saveLog("3....B............B............B............B............B", whitePlayer);
 
-    cache.writeLog("1....B............B............B............B............B", whitePlayer);
-    cache.writeLog("2....B............B............B............B............B", whitePlayer);
-    cache.writeLog("3....B............B............B............B............B", whitePlayer);
-
+    CacheHandler newCache = new CacheHandler();
+    // now we don't have logs in memory, but they are stored in the file
+    newCache.loadLogsInMemoryFromDisk();
+    assertEquals(3, newCache.getLogCounter());
     assertEquals(
-        "3....B............B............B............B............B", cache.readLastLog().board());
-
-    // close the program
-    CacheHandler cache1 = new CacheHandler();
-    Player blackPlayer = new Player(BoardPoint.BLACK);
-    cache.writeLog("4....B............B............B............B............B", blackPlayer);
+        "1....B............B............B............B............B", newCache.readLog(0).board());
     assertEquals(
-        "4....B............B............B............B............B", cache.readLastLog().board());
+        "2....B............B............B............B............B", newCache.readLog(1).board());
+    assertEquals(
+        "3....B............B............B............B............B", newCache.readLog(2).board());
+    assertEquals("W", cache.readLastLog().nextMove());
+  }
+
+  @Test
+  public void testBackAndMove() {
+    CacheHandler cache = new CacheHandler();
+    Player whitePlayer = new Player(BoardPoint.WHITE);
+    cache.clearCache();
+    cache.saveLog("1....B............B............B............B............B", whitePlayer);
+    cache.saveLog("2....B............B............B............B............B", whitePlayer);
+    cache.saveLog("3....B............B............B............B............B", whitePlayer);
+    cache.saveLog("4....B............B............B............B............B", whitePlayer);
+    cache.saveLog("5....B............B............B............B............B", whitePlayer);
+    cache.saveLog("6....B............B............B............B............B", whitePlayer);
+    cache.saveLog("7....B............B............B............B............B", whitePlayer);
+
+    /*
+    RESULT in memory
+    moveIndex 0: 1....B............B............B............B............B
+    moveIndex 1: 2....B............B............B............B............B
+    moveIndex 2: 3....B............B............B............B............B
+    moveIndex 3: 4....B............B............B............B............B
+    moveIndex 4: 5....B............B............B............B............B
+    moveIndex 5: 6....B............B............B............B............B
+    moveIndex 6: 7....B............B............B............B............B
+
+    7 moves
+
+    RESULT in file
+    20241212_153312 1....B............B............B............B............B W
+    20241212_153312 2....B............B............B............B............B W
+    20241212_153312 3....B............B............B............B............B W
+    20241212_153312 4....B............B............B............B............B W
+    20241212_153312 5....B............B............B............B............B W
+    20241212_153312 6....B............B............B............B............B W
+    20241212_153312 7....B............B............B............B............B W
+    */
+
+    assertEquals(7, cache.getLogCounter());
+    assertEquals(6, cache.getMoveIndex());
+    assertEquals(7, cache.calculateMoveCounter());
+
     assertEquals(
         "4....B............B............B............B............B", cache.readLog(3).board());
+    // If I invoke readLog, I change the moveIndex
+    assertEquals(3, cache.getMoveIndex());
+    // I go back in the game, so there are fewer moves
+    assertEquals(4, cache.calculateMoveCounter());
 
-    cache1.clearCache(); // for the next test
+    cache.saveLog("new....B............B............B............B............B", whitePlayer);
+    //    // If I invoke saveLog, I invoke removeElementsAfterIndex
+    assertEquals(4, cache.getMoveIndex());
+    assertEquals(5, cache.getLogCounter());
+    // The old moves after that are lost, because we changed move, and so the move made is the last
+    // one
+    assertEquals(5, cache.calculateMoveCounter());
+
+    /*
+    RESULT in memory
+      20241212_144554 1....B............B............B............B............B W
+      20241212_144554 2....B............B............B............B............B W
+      20241212_144554 3....B............B............B............B............B W
+      20241212_144554 4....B............B............B............B............B W
+      20241212_144554 new....B............B............B............B............B W
+
+      5 moves
+
+    RESULT in file
+      20241212_150514 new....B............B............B............B............B W
+     */
+    cache.clearCache();
   }
 }
