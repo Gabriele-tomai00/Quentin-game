0a1eefa437a8ddb6f6f50b1a899e81fcaf3aff4c
alequerenghi <querenghialessandro@icloud.com>
2024-12-13 16:28:43 +0100

    applied spotless

--- src/main/java/quentin/Main.java
+++ src/main/java/quentin/Main.java
@@ -12,10 +12,13 @@ public class Main {
     System.out.println("   ____  _    _            _   _          _____                      ");
     System.out.println("  / __ \\| |  | |          | | (_)        / ____|                     ");
     System.out.println(" | |  | | |  | | ___ _ __ | |_ _ _ __   | |  __  __ _ _ __ ___   ___ ");
-    System.out.println(" | |  | | |  | |/ _ \\ '_ \\| __| | '_ \\  | | |_ |/ _` | '_ ` _ \\ / _ \\");
+    System.out.println(
+        " | |  | | |  | |/ _ \\ '_ \\| __| | '_ \\  | | |_ |/ _` | '_ ` _ \\ / _ \\");
     System.out.println(" | |__| | |__| |  __/ | | | |_| | | | | | |__| | (_| | | | | | |  __/");
-    System.out.println("  \\___\\_\\\\____/ \\___|_| |_|\\__|_|_| |_|  \\_____|\\__,_|_| |_| |_|\\___|");
-    System.out.println(" by Luis Bolaños Mures                                                          ");
+    System.out.println(
+        "  \\___\\_\\\\____/ \\___|_| |_|\\__|_|_| |_|  \\_____|\\__,_|_| |_| |_|\\___|");
+    System.out.println(
+        " by Luis Bolaños Mures                                                          ");
 
     try (Scanner scanner = new Scanner(System.in)) {
       while (true) {
@@ -34,17 +37,13 @@ public class Main {
           System.out.print(CLEAR);
           System.out.println(game.getBoard());
         }
-        if (game.getBoard()
-                .hasWon(game.getCurrenPlayer()
-                            .color())) {
+        if (game.getBoard().hasWon(game.getCurrenPlayer().color())) {
           System.out.print(CLEAR);
           System.out.printf("%s has won", game.getCurrenPlayer());
           break;
         }
         game.changeCurrentPlayer();
-        if (game.getBoard()
-                .hasWon(game.getCurrenPlayer()
-                            .color())) {
+        if (game.getBoard().hasWon(game.getCurrenPlayer().color())) {
           System.out.print(CLEAR);
           System.out.printf("%s has won", game.getCurrenPlayer());
           break;
