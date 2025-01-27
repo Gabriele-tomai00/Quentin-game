61055232de7011843e1c3bbc8fb3106839f4e577
alequerenghi <querenghialessandro@icloud.com>
2025-01-27 22:38:16 +0100

    refactor: cell now is a record as always should have been

--- app/src/main/java/quentin/game/Cell.java
+++ app/src/main/java/quentin/game/Cell.java
@@ -2,50 +2,22 @@ package quentin.game;
 
 import java.util.Objects;
 
-public class Cell {
+public record Cell(int row, int col) {
 
-    private int row;
-    private int col;
+  @Override
+  public int hashCode() {
+    return Objects.hash(col, row);
+  }
 
-    public Cell(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
+  @Override
+  public final boolean equals(Object obj) {
+    if (this == obj) { return true; }
+    if (obj instanceof Cell(int row, int col)) { return row == row() && col == col(); }
+    return false;
+  }
 
-    public int row() {
-        return row;
-    }
-
-    public int col() {
-        return col;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public void setCol(int col) {
-        this.col = col;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(col, row);
-    }
-
-    @Override
-    public final boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj instanceof Cell cell) {
-            return cell.row == this.row && cell.col == this.col;
-        }
-        return false;
-    }
-
-    @Override
-    public final String toString() {
-        return String.format("(%c, %d)", col + 'a', row + 1);
-    }
+  @Override
+  public final String toString() {
+    return String.format("(%c, %d)", col + 'a', row + 1);
+  }
 }
