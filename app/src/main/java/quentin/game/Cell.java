9d59961daa4144642e204325d2a860baa29cb541
Gabriele <tomaigabriele@gmail.com>
2025-01-28 10:39:23 +0100

    refactor: new function appendBoardPoint to simplify toString() method and small refactor in Cell

--- app/src/main/java/quentin/game/Cell.java
+++ app/src/main/java/quentin/game/Cell.java
@@ -10,18 +10,18 @@ public record Cell(int row, int col) {
     }
 
     @Override
-    public final boolean equals(Object obj) {
+    public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-        if (obj instanceof Cell(int row, int col)) {
-            return row == row() && col == col();
+        if (obj instanceof Cell(int otherRow, int otherCol)) {
+            return row == otherRow && col == otherCol;
         }
         return false;
     }
 
     @Override
-    public final String toString() {
+    public String toString() {
         return String.format("(%c, %d)", col + 'a', row + 1);
     }
 }
