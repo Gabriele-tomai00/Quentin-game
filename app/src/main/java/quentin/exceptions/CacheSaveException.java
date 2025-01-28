45106c72b38c3fc56b289b85fce9911a648fce58
alequerenghi <querenghialessandro@icloud.com>
2025-01-28 16:00:27 +0100

    added missing serial version id to excpetion classes

--- app/src/main/java/quentin/exceptions/CacheSaveException.java
+++ app/src/main/java/quentin/exceptions/CacheSaveException.java
@@ -1,6 +1,8 @@
 package quentin.exceptions;
 
 public class CacheSaveException extends RuntimeException {
+    private static final long serialVersionUID = 3311314235858597788L;
+
     public CacheSaveException(String message) {
         super(message);
     }
