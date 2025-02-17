c0d3653f3cc2687017a607045f2c01c40aa80801
alequerenghi <querenghialessandro@icloud.com>
2025-02-17 14:35:14 +0100

    Testing starters

---
+++ app/src/test/java/quentin/game/SimpleGameStarterTest.java
@@ -0,0 +1,42 @@
+package quentin.game;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleGameStarterTest {
+
+  void provideInput(String data) {
+    ByteArrayInputStream is = new ByteArrayInputStream(data.getBytes());
+    System.setIn(is);
+  }
+
+  /*
+   * @Test void testExit() { provideInput("exit\n"); GameStarter starter = new
+   * CachedGameStarter(); starter.start(); assertTrue(true); }
+   */
+
+  @Test
+  void testBlackWin() {
+    StringBuilder builder = new StringBuilder();
+    for (int row = 1; row <= 13; row++) {
+      builder.append("a" + row + "\n");
+      builder.append("c" + row + "\n");
+    }
+    provideInput(builder.toString());
+    SimpleGameStarter starter = new SimpleGameStarter();
+    starter.start();
+    assertAll(() -> assertEquals(BoardPoint.BLACK, starter.getGame()
+                                                          .getBoard()
+                                                          .getPoint(new Cell(0, 0))),
+              () -> assertEquals(BoardPoint.BLACK, starter.getGame()
+                                                          .getBoard()
+                                                          .getPoint(new Cell(12, 0))),
+              () -> assertTrue(starter.hasWon()));
+  }
+
+}
