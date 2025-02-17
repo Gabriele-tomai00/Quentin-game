4d63af0d34aafce9b5a8ee1c83c2b3bb47a0d043
alequerenghi <querenghialessandro@icloud.com>
2025-02-17 14:35:14 +0100

    Testing starters

--- app/src/main/java/quentin/game/SimpleGameStarter.java
+++ app/src/main/java/quentin/game/SimpleGameStarter.java
@@ -4,63 +4,61 @@ import java.util.Scanner;
 
 public class SimpleGameStarter implements GameStarter {
 
-    protected LocalGame game;
+  protected LocalGame game;
+  private Scanner scanner;
 
-    public SimpleGameStarter() {
-        game = new LocalGame();
-    }
+  public SimpleGameStarter() {
+    game = new LocalGame();
+    scanner = new Scanner(System.in);
+  }
 
-    @Override
-    public void start() {
-        display();
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                if (!game.canPlayerPlay()) {
-                    game.changeCurrentPlayer();
-                } else {
-                    if (processInput(scanner)) {
-                        break;
-                    }
-                    display();
-                }
-            }
-        }
-    }
-
-    public boolean processInput(Scanner scanner) {
-        while (true) {
-            displayMessage(String.format("%s > ", game.getCurrentPlayer()));
-            try {
-                makeMove(scanner.next());
-                break;
-            } catch (RuntimeException e) {
-                displayMessage(e.getMessage());
-            }
-        }
-        return hasWon();
-    }
-
-    public boolean hasWon() {
-        if (game.hasWon(game.getCurrentPlayer())) {
-            displayWinner();
-            return true;
-        }
+  @Override
+  public void start() {
+    display();
+    while (true) {
+      if (!game.canPlayerPlay()) {
         game.changeCurrentPlayer();
-        if (game.hasWon(game.getCurrentPlayer())) {
-            displayWinner();
-            return true;
-        }
-        return false;
+      } else {
+        if (processInput(scanner)) { break; }
+        display();
+      }
     }
+  }
 
-    public void makeMove(String input) {
-        Cell cell = new MoveParser(input).parse();
-        game.place(cell);
-        game.coverTerritories(cell);
+  public boolean processInput(Scanner scanner) {
+    while (true) {
+      displayMessage(String.format("%s > ", game.getCurrentPlayer()));
+      try {
+        makeMove(scanner.nextLine());
+        break;
+      } catch (RuntimeException e) {
+        displayMessage(e.getMessage());
+      }
     }
+    return hasWon();
+  }
 
-    @Override
-    public Game getGame() {
-        return game;
+  public boolean hasWon() {
+    if (game.hasWon(game.getCurrentPlayer())) {
+      displayWinner();
+      return true;
     }
+    game.changeCurrentPlayer();
+    if (game.hasWon(game.getCurrentPlayer())) {
+      displayWinner();
+      return true;
+    }
+    return false;
+  }
+
+  public void makeMove(String input) {
+    Cell cell = new MoveParser(input).parse();
+    game.place(cell);
+    game.coverTerritories(cell);
+  }
+
+  @Override
+  public Game getGame() {
+    return game;
+  }
 }
