7bf189ae8a91391e00d2ae7cdb4f469d6ee87f5f
alequerenghi <117732128+alequerenghi@users.noreply.github.com>
2025-03-01 11:20:21 -0800

    rework UdpClient and UdpServer

--- app/src/main/java/quentin/network/UDPClient.java
+++ app/src/main/java/quentin/network/UDPClient.java
@@ -4,48 +4,32 @@ import java.net.*;
 
 public class UDPClient {
     private static final int UDP_SERVER_PORT = 9876;
-    private Boolean discovery = true;
-    private Thread discoveryThread;
-    private ServerInfo tcpServerInfo;
-    private Runnable onDiscoveredCallback;
+    private boolean discovery = true;
     private final String clientAddress;
 
     public UDPClient() {
         clientAddress = CorrectAddressGetter.getLocalIpAddress();
     }
 
-    public ServerInfo getTcpServerInfo() {
-        return tcpServerInfo;
-    }
-
-    public void startDiscovery() {
+    public tcpServerInfo call() {
         System.out.println("address of udp client: " + clientAddress);
-        discoveryThread =
-                new Thread(
-                        () -> {
-                            try {
-                                InetAddress localAddress = InetAddress.getByName(clientAddress);
-                                SocketAddress bindAddress = new InetSocketAddress(localAddress, 0);
-                                try (DatagramSocket clientSocket =
-                                        new DatagramSocket(bindAddress)) {
-                                    System.out.println(
-                                            "C: client discovery started using address: "
-                                                    + clientAddress);
-                                    clientSocket.setBroadcast(true);
-                                    handleDiscovery(clientSocket);
-                                }
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                                System.err.println(
-                                        "Error stopping UDP client: Thread was interrupted");
-                            } catch (Exception e) {
-                                System.err.println("Unexpected error in udp client discovery");
-                            }
-                        });
-        discoveryThread.start();
+        try {
+            InetAddress localAddress = InetAddress.getByName(clientAddress);
+            SocketAddress bindAddress = new InetSocketAddress(localAddress, 0);
+            try (DatagramSocket clientSocket =
+                    new DatagramSocket(bindAddress)) {
+                System.out.println(
+                        "C: client discovery started using address: "
+                                + clientAddress);
+                clientSocket.setBroadcast(true);
+                return handleDiscovery(clientSocket);
+            }
+        } catch (Exception e) {
+            System.err.println("Unexpected error in udp client discovery");
+        }
     }
 
-    private void handleDiscovery(DatagramSocket clientSocket) throws InterruptedException {
+    private tcpServerInfo handleDiscovery(DatagramSocket clientSocket) throws InterruptedException {
         byte[] sendBuffer = "Requesting server information".getBytes();
         byte[] receiveBuffer = new byte[1024];
         while (discovery) {
@@ -65,34 +49,19 @@ public class UDPClient {
 
                 DatagramPacket receivePacket =
                         new DatagramPacket(receiveBuffer, receiveBuffer.length);
-                clientSocket.setSoTimeout(5000);
+                clientSocket.setSoTimeout(1000);
                 clientSocket.receive(receivePacket);
 
-                tcpServerInfo = ServerInfo.fromBytes(receivePacket.getData());
-                System.out.println("Received ServerInfo: " + tcpServerInfo);
-                if (onDiscoveredCallback != null) onDiscoveredCallback.run();
-
-                break;
+                return ServerInfo.fromBytes(receivePacket.getData());
+            } catch (SocketTimeOutException e) {
+                // do nothing
             } catch (Exception e) {
-                Thread.sleep(400);
+                e.printStackTrace();
             }
         }
     }
 
     public void stopDiscovery() {
         discovery = false;
-        try {
-            discoveryThread.join();
-            if (!discoveryThread.isAlive()) {
-                System.out.println("client discovery correctly stopped");
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.err.println("Error stopping udp client discovery");
-        }
-    }
-
-    public void setOnDiscoveredCallback(Runnable callback) {
-        this.onDiscoveredCallback = callback;
     }
 }
--- app/src/main/java/quentin/network/UDPServer.java
+++ app/src/main/java/quentin/network/UDPServer.java
@@ -19,76 +19,57 @@ public class UDPServer {
         address = CorrectAddressGetter.getLocalIpAddress();
     }
 
-    public void startServer() {
-        discoveryThread =
-                new Thread(
-                        () -> {
-                            try (DatagramSocket serverSocket =
-                                    new DatagramSocket(UDP_SERVER_PORT)) {
-                                System.out.println(
-                                        "UDP Server is listening: ip "
-                                                + address
-                                                + " port "
-                                                + UDP_SERVER_PORT);
-                                discovery = true;
-                                serverSocket.setSoTimeout(1000);
-
-                                while (discovery) {
-                                    receiveAndRespondUdp(serverSocket);
-                                }
-                            } catch (Exception e) {
-                                System.err.println(
-                                        "Error during server socket initialization in UDP server");
-                            }
-                        });
-        discoveryThread.start();
+    public void run() {
+        try (DatagramSocket serverSocket = new DatagramSocket(UDP_SERVER_PORT)) {
+            System.out.println(
+                        "UDP Server is listening: ip "
+                                + address
+                                + " port "
+                                + UDP_SERVER_PORT);
+            discovery = true;
+            serverSocket.setSoTimeout(1000);
+            receiveAndRespondUdp(serverSocket)
+            } catch (Exception e) {
+                System.err.println(
+                        "Error during server socket initialization in UDP server");
+            }
+        }
     }
 
     public void receiveAndRespondUdp(DatagramSocket serverSocket) {
         byte[] receiveBuffer = new byte[1024];
-        try {
-            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
-            serverSocket.receive(receivePacket);
+        while (discovery) {
+            try {
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                serverSocket.receive(receivePacket);
 
-            InetAddress clientAddress = receivePacket.getAddress();
-            int clientPort = receivePacket.getPort();
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
 
-            System.out.println(
-                    "UDP: Received request from client " + clientAddress + ":" + clientPort);
-            ServerInfo serverInfo = new ServerInfo(address, tcpPort, username);
+                System.out.println(
+                        "UDP: Received request from client " + clientAddress + ":" + clientPort);
+                ServerInfo serverInfo = new ServerInfo(address, tcpPort, username);
 
-            byte[] sendBuffer = serverInfo.toBytes();
+                byte[] sendBuffer = serverInfo.toBytes();
 
-            DatagramPacket sendPacket =
-                    new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress, clientPort);
-            serverSocket.send(sendPacket);
+                DatagramPacket sendPacket =
+                        new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress, clientPort);
+                serverSocket.send(sendPacket);
 
-            System.out.println(
-                    "UDP: Response sent to client: "
-                            + serverInfo
-                            + " clientAddress: "
-                            + clientAddress);
-        } catch (SocketTimeoutException e) {
-            if (!discovery) {
-                System.out.println("Timeout occurred, stopping server.");
+                System.out.println(
+                        "UDP: Response sent to client: "
+                                + serverInfo
+                                + " clientAddress: "
+                                + clientAddress);
+            } catch (SocketTimeoutException e) {
+                // do nothing
+            } catch (Exception e) {
+                System.err.println("Unexpected error occurred in UDP server");
             }
-        } catch (Exception e) {
-            System.err.println("Unexpected error occurred in UDP server");
         }
     }
 
     public void stopServer() {
-        if (discovery) {
-            discovery = false;
-            try {
-                discoveryThread.join();
-                System.out.println("UDP server successfully stopped");
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                System.err.println("Error stopping UDP server: Thread was interrupted");
-            } catch (Exception e) {
-                System.err.println("Unexpected error while stopping UDP server: " + e.getMessage());
-            }
-        }
+        discovery = false;
     }
 }
