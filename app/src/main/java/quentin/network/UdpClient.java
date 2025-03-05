b28397bbb58b581b7f113c536cb0c9b5b2b18100
alequerenghi <querenghialessandro@icloud.com>
2025-03-05 12:07:22 +0100

    Tested UdpClient

--- app/src/main/java/quentin/network/UdpClient.java
+++ app/src/main/java/quentin/network/UdpClient.java
@@ -8,54 +8,44 @@ import java.net.InetSocketAddress;
 import java.net.SocketTimeoutException;
 
 public class UdpClient {
-    private static final int UDP_SERVER_PORT = 9876;
-    private boolean discovery = true;
-    private final String clientAddress;
+  private static final int UDP_SERVER_PORT = 9876;
+  private boolean discovery = true;
+  private final String clientAddress;
 
-    public UdpClient() {
-        clientAddress = CorrectAddressGetter.getLocalIpAddress();
+  public UdpClient() {
+    clientAddress = CorrectAddressGetter.getLocalIpAddress();
+  }
+
+  public ServerInfo run() {
+    while (discovery) {
+//    System.out.println("address of udp client: " + clientAddress);
+      try (DatagramSocket clientSocket = new DatagramSocket(
+          new InetSocketAddress(InetAddress.getByName(clientAddress), 0))) {
+        clientSocket.setBroadcast(true);
+
+        byte[] buffer = "GET INFO".getBytes();
+        InetAddress broadcastAddress = InetAddress.getByName("255.255.255.255");
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, broadcastAddress, UDP_SERVER_PORT);
+        clientSocket.send(packet);
+
+//        System.out.println("Broadcast request sent (to: " + broadcastAddress + " port: " + UDP_SERVER_PORT + ")");
+
+        buffer = new byte[1024];
+        DatagramPacket recPacket = new DatagramPacket(buffer, buffer.length);
+        clientSocket.setSoTimeout(1000);
+        clientSocket.receive(recPacket);
+        String info = new String(recPacket.getData(), 0, recPacket.getLength());
+        return ServerInfo.fromString(info);
+      } catch (SocketTimeoutException e) {
+        // do nothing
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+    return null;
+  }
 
-    public ServerInfo run() {
-        System.out.println("address of udp client: " + clientAddress);
-        try (DatagramSocket clientSocket =
-                new DatagramSocket(
-                        new InetSocketAddress(InetAddress.getByName(clientAddress), 0))) {
-            System.out.println("C: client discovery started using address: " + clientAddress);
-            clientSocket.setBroadcast(true);
-
-            byte[] sendBuffer = "Requesting server information".getBytes();
-            byte[] receiveBuffer = new byte[1024];
-            while (discovery) {
-                InetAddress broadcastAddress = InetAddress.getByName("255.255.255.255");
-                DatagramPacket sendPacket =
-                        new DatagramPacket(
-                                sendBuffer, sendBuffer.length, broadcastAddress, UDP_SERVER_PORT);
-                clientSocket.send(sendPacket);
-
-                System.out.println(
-                        "Broadcast request sent (to: "
-                                + broadcastAddress
-                                + " port: "
-                                + UDP_SERVER_PORT
-                                + ")");
-
-                DatagramPacket receivePacket =
-                        new DatagramPacket(receiveBuffer, receiveBuffer.length);
-                clientSocket.setSoTimeout(1000);
-                clientSocket.receive(receivePacket);
-
-                return ServerInfo.fromBytes(receivePacket.getData());
-            }
-        } catch (SocketTimeoutException e) {
-            // do nothing
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public void stopDiscovery() {
-        discovery = false;
-    }
+  public void stop() {
+    discovery = false;
+  }
 }
