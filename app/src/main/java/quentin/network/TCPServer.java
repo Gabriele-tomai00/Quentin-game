693f191dfdbc35eefeedcbe10ff00fbce3f17496
alequerenghi <querenghialessandro@icloud.com>
2025-03-05 12:08:38 +0100

    Tested TcpServer

--- app/src/main/java/quentin/network/TCPServer.java
+++ app/src/main/java/quentin/network/TCPServer.java
@@ -8,37 +8,37 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPServer {
-    private final String password;
-    private final int port;
+  private final String password;
+  private final int port;
 
-    public TCPServer(int port, String password) {
-        this.port = port;
-        this.password = password;
-    }
+  public TCPServer(int port, String password) {
+    this.port = port;
+    this.password = password;
+  }
 
-    public Socket start() {
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            Socket socket = serverSocket.accept(); // Blocking call: waits for a client to connect
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println("CODE: " + password);
-            String message;
-            for (int attempt = 0; attempt < 3; attempt++) {
-                if ((message = in.readLine()) != null) {
-                    if (!message.equals(password)) {
-                        System.out.println(
-                                "Invalid password, retry (attempt " + (attempt + 1) + "/3)");
-                        out.println("SERVER ERR");
-                    } else {
-                        System.out.println("Password of TCP client accepted");
-                        out.println("SERVER OK");
-                        return socket;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+  public Socket start() {
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+      Socket socket = serverSocket.accept();
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      System.out.println("CODE: " + password);
+      String message;
+      for (int attempt = 0; attempt < 3; attempt++) {
+        message = in.readLine();
+        if (message != null) {
+          if (!message.equals(password)) {
+//            System.out.println("Invalid password, retry (attempt " + (attempt + 1) + "/3)");
+            out.println("ACCESS DENIED");
+          } else {
+            System.out.println("PASS OK");
+            out.println("SERVER OK");
+            return socket;
+          }
         }
-        return null;
+      }out.println("SERVER ERR");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 }
---
+++ app/src/test/java/quentin/network/TCPServerTest.java
@@ -0,0 +1,42 @@
+package quentin.network;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TCPServerTest {
+
+  private static TCPServer tcpServer;
+  private static Future<Socket> submit;
+
+  @BeforeAll
+  static void startServer() {
+    tcpServer = new TCPServer(2000, "pass");
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    submit = executor.submit(tcpServer::start);
+  }
+
+  @Test
+  void testTcpServer() throws InterruptedException, ExecutionException, IOException {
+    try (Socket socket = new Socket(InetAddress.getLocalHost(), 2000)) {
+      PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+      writer.println("1");
+      writer.println("2");
+      writer.println("pass");
+      Socket serverSocket = submit.get();
+      assertAll(() -> assertTrue(serverSocket.isBound()), () -> assertEquals(2000, serverSocket.getLocalPort()));
+    }
+  }
+}
