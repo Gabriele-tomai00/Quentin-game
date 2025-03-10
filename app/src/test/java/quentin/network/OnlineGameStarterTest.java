16453f24e3458905701c195a27deca3e26198d90
alequerenghi <querenghialessandro@icloud.com>
2025-03-10 15:33:07 +0100

    Tested Online starter

---
+++ app/src/test/java/quentin/network/OnlineGameStarterTest.java
@@ -0,0 +1,77 @@
+package quentin.network;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import quentin.game.BoardPoint;
+import quentin.game.Cell;
+import quentin.game.Player;
+
+class OnlineGameStarterTest {
+
+  private ByteArrayOutputStream outputErr = new ByteArrayOutputStream();
+  private ByteArrayOutputStream output = new ByteArrayOutputStream();
+  private NetworkHandler handler = new NetworkHandler(null, null) {
+    @Override
+    public synchronized void sendCommands(String command) {
+      // do nothing
+    }
+  };
+
+  @BeforeEach
+  void setOutput() {
+    System.setErr(new PrintStream(outputErr));
+    System.setOut(new PrintStream(output));
+  }
+
+  void provideInput(String data) {
+    ByteArrayInputStream is = new ByteArrayInputStream(data.getBytes());
+    System.setIn(is);
+  }
+
+  @Test
+  void testPieRule() {
+    String commands = "pie\nexit\n";
+    provideInput(commands);
+    OnlineGameStarter starter = new OnlineGameStarter(handler, new OnlineGame(new Player(BoardPoint.WHITE)));
+    starter.run();
+    BoardPoint color = starter.getGame()
+                              .getCurrentPlayer()
+                              .color();
+    assertEquals(BoardPoint.BLACK, color);
+  }
+
+  @Test
+  void testPieRuleNotPossible() {
+    String commands = "pie\nexit\n";
+    provideInput(commands);
+    OnlineGameStarter starter = new OnlineGameStarter(handler, new OnlineGame(new Player(BoardPoint.BLACK)));
+    starter.run();
+    BoardPoint color = starter.getGame()
+                              .getCurrentPlayer()
+                              .color();
+    String errorMessage = "Cannot apply pie rule!";
+    assertAll(() -> assertEquals(BoardPoint.BLACK, color), () -> assertTrue(outputErr.toString()
+                                                                                     .contains(errorMessage)));
+  }
+
+  @Test
+  void placeOneTileTest() {
+    String commands = "a1\nexit\n";
+    provideInput(commands);
+    OnlineGameStarter starter = new OnlineGameStarter(handler, new OnlineGame(new Player(BoardPoint.BLACK)));
+    starter.run();
+    BoardPoint cell = BoardPoint.BLACK;
+    assertEquals(cell, starter.getGame()
+                              .getBoard()
+                              .getPoint(new Cell(0, 0)));
+  }
+}
