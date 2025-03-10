7dd564943955b55e1146571cfeef08a92b38b00b
alequerenghi <querenghialessandro@icloud.com>
2025-03-10 10:47:10 +0100

    Looking into a solution for Udp testing

--- app/src/test/java/quentin/network/UdpServerTest.java
+++ app/src/test/java/quentin/network/UdpServerTest.java
@@ -6,36 +6,39 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class UdpServerTest {
-    private static UdpServer server;
+  private static UdpServer server;
+  private static final int PORT= 9876;
 
-    @BeforeAll
-    static void startServer() {
-        server = new UdpServer("default", 9876);
-        new Thread(server::run).start();
-    }
+  @BeforeAll
+  static void startServer() {
+    server = new UdpServer("default", PORT);
+    new Thread(server::run).start();
+  }
 
-    @AfterAll
-    static void stopServer() {
-        server.stop();
-    }
+  @AfterAll
+  static void stopServer() {
+    server.stop();
+  }
 
-    @Test
-    void testServer() throws IOException {
-        try (DatagramSocket datagramSocket = new DatagramSocket()) {
-            byte[] buff = "mess".getBytes();
-            DatagramPacket packet =
-                    new DatagramPacket(buff, buff.length, InetAddress.getLocalHost(), 9876);
-            datagramSocket.send(packet);
-            buff = new byte[1024];
-            packet = new DatagramPacket(buff, buff.length);
-            datagramSocket.receive(packet);
-            String data = new String(packet.getData(), 0, packet.getLength());
-            assertEquals("10.110.40.198 - default", data);
-        }
+  @Test
+  void testServer() throws IOException {
+    try (DatagramSocket datagramSocket = new DatagramSocket()) {
+      byte[] buff = "mess".getBytes();
+      DatagramPacket packet = new DatagramPacket(buff, buff.length, InetAddress.getByName("localhost"), PORT);
+      datagramSocket.send(packet);
+      buff = new byte[1024];
+      packet = new DatagramPacket(buff, buff.length);
+      datagramSocket.receive(packet);
+      String data = new String(packet.getData(), 0, packet.getLength());
+      assertEquals(InetAddress.getLocalHost()
+                              .getHostAddress()
+          + " - default", data);
     }
+  }
 }
