6c00408382ed27468af4cbae3ffcae149bc75926
alequerenghi <querenghialessandro@icloud.com>
2025-03-13 21:33:00 +0100

    Testing gui

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -3,11 +3,8 @@ module quentin.app {
     requires javafx.fxml;
     requires transitive javafx.graphics;
 
-    exports quentin.game;
-    exports quentin.cache;
-    exports quentin.gui;
-    exports quentin;
-
     opens quentin.gui to
             javafx.fxml;
+
+    exports quentin;
 }
