800cda1bc7be2e94781ddc250f7e0f2864206880
alequerenghi <querenghialessandro@icloud.com>
2025-04-02 10:24:54 +0200

    fixing tests

--- app/src/main/java/quentin/cache/CacheHandler.java
+++ app/src/main/java/quentin/cache/CacheHandler.java
@@ -8,66 +8,56 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Objects;
-import quentin.exceptions.CacheClearException;
-import quentin.exceptions.CacheSaveException;
-import quentin.game.GameBoard;
 
 public class CacheHandler {
-    private static final String GAME_DIR = ".quentinGame";
-    private static final String CACHE_FILE = GAME_DIR + "/last_match_cache.dat";
+  private static final String CACHE_FILE = ".last_match_cache";
 
-    private CacheHandler() {
-        throw new UnsupportedOperationException(
-                "CacheHandler is a utility class and cannot be instantiated.");
+  private CacheHandler() {
+    throw new UnsupportedOperationException("CacheHandler is a utility class and cannot be instantiated.");
+  }
+
+  public static Cache<GameLog> initialize() {
+    return initialize(new File(CACHE_FILE));
+  }
+
+  @SuppressWarnings("unchecked")
+  public static Cache<GameLog> initialize(File file) {
+    Cache<GameLog> cache = new Cache<>();
+
+    if (file.exists()) {
+      try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(file))) {
+        cache = (Cache<GameLog>) input.readObject();
+        System.out.println("Cache loaded successfully from " + file);
+      } catch (IOException e) {
+        System.err.println("Failed to load cache due to an IO error: " + e.getMessage());
+      } catch (ClassNotFoundException e) {
+        System.err.println("Failed to load cache: Class not found. This may indicate incompatible" + " serialization.");
+      }
+    } else {
+      System.out.println("Cache file does not exist. Using a new cache.");
     }
+    return cache;
+  }
 
-    @SuppressWarnings("unchecked")
-    public static Cache<GameLog> initialize() {
-        Cache<GameLog> cache = new Cache<>();
-        File file = new File(CACHE_FILE);
+  public static void saveCache(Cache<GameLog> cache) {
+    saveCache(new File(CACHE_FILE), cache);
+  }
 
-        if (file.exists()) {
-            try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(file))) {
-                cache = (Cache<GameLog>) input.readObject();
-                System.out.println("Cache loaded successfully from " + CACHE_FILE);
-            } catch (IOException e) {
-                System.err.println("Failed to load cache due to an IO error: " + e.getMessage());
-            } catch (ClassNotFoundException e) {
-                System.err.println(
-                        "Failed to load cache: Class not found. This may indicate incompatible"
-                                + " serialization.");
-            }
-        } else {
-            System.out.println("Cache file does not exist. Using a new cache.");
-        }
-
-        return cache;
+  public static void saveCache(File file, Cache<GameLog> cache) {
+    if (cache.getMemorySize() <= 1) { return; }
+    try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file))) {
+      output.writeObject(cache);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public static void saveCache(Cache<GameLog> cache) {
-        if (cache.getMemorySize() == 1
-                && Objects.equals(cache.getLog().game().getBoard(), new GameBoard())) {
-            return;
-        }
-        File cacheDirectory = new File(GAME_DIR);
-        if (!cacheDirectory.exists() && !cacheDirectory.mkdirs()) {
-            throw new CacheSaveException("Failed to create cache directory: " + GAME_DIR);
-        }
-        clearCache();
-        try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(CACHE_FILE))) {
-            output.writeObject(cache);
-        } catch (IOException e) {
-            System.out.println("Failed to save cache: " + CACHE_FILE);
-        }
+  public static void clearCache() {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(CACHE_FILE))) {
+      writer.write("");
+      writer.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    public static void clearCache() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(CACHE_FILE))) {
-            writer.write("");
-            writer.flush();
-        } catch (IOException e) {
-            throw new CacheClearException("Failed to clear the cache file: " + CACHE_FILE, e);
-        }
-    }
+  }
 }
--- app/src/test/java/quentin/game/BoardTest.java
+++ app/src/test/java/quentin/game/BoardTest.java
@@ -8,55 +8,57 @@ import org.junit.jupiter.api.Test; // Test of JUnit 5
 
 class BoardTest {
 
-    GameBoard board = new GameBoard();
+  GameBoard board = new GameBoard();
 
-    @Test
-    void testCorrectValuesInMatrix() {
-        board.placeStone(BoardPoint.BLACK, 2, 2);
-        board.placeStone(BoardPoint.WHITE, 2, 3);
-        assertDoesNotThrow(() -> board);
-    }
+  @Test
+  void testCorrectValuesInMatrix() {
+    assertDoesNotThrow(() -> board.placeStone(BoardPoint.BLACK, 2, 2));
+    assertDoesNotThrow(() -> board.placeStone(BoardPoint.WHITE, 2, 3));
+  }
 
-    @Test
-    void testCorrectPlaceStone() {
-        board.placeStone(BoardPoint.BLACK, 1, 2);
-        board.placeStone(BoardPoint.WHITE, 3, 4);
-        assertAll(
-                () -> assertEquals(BoardPoint.BLACK, board.getPoint(new Cell(1, 2))),
-                () -> assertEquals(BoardPoint.WHITE, board.getPoint(new Cell(3, 4))));
-    }
+  @Test
+  void testCorrectPlaceStone() {
+    board.placeStone(BoardPoint.BLACK, 1, 2);
+    board.placeStone(BoardPoint.WHITE, 3, 4);
+    assertAll(() -> assertEquals(BoardPoint.BLACK, board.getPoint(new Cell(1, 2))),
+              () -> assertEquals(BoardPoint.WHITE, board.getPoint(new Cell(3, 4))));
+  }
 
-    @Test
-    void testToCompactStringAndFromCompactString() {
-        for (int i = 0; i < board.size(); i++) {
-            board.placeStone(BoardPoint.BLACK, i, 4);
-        }
-        String expectedResult = board.toCompactString();
-        assertEquals(expectedResult, board.toCompactString());
+  @Test
+  void testEmptyBoardCompactString() {
+    String expectedResult = "169";
+    assertEquals(expectedResult, board.toCompactString());
 
-        GameBoard newBoard = new GameBoard();
-        newBoard.fromCompactString(expectedResult);
-        assertEquals(expectedResult, newBoard.toCompactString());
+  }
 
-        board = new GameBoard();
-        expectedResult = "169";
-        assertEquals(expectedResult, board.toCompactString());
-        board = new GameBoard();
-        board.placeStone(BoardPoint.BLACK, 2, 3);
-        board.placeStone(BoardPoint.BLACK, 4, 5);
-        expectedResult = "29B27B111";
-        assertEquals(expectedResult, board.toCompactString());
-        newBoard = new GameBoard();
-        newBoard.fromCompactString(expectedResult);
-        assertEquals(expectedResult, newBoard.toCompactString());
-        assertEquals(newBoard, board);
-    }
+  @Test
+  void testCompactString2Tiles() {
+    board.placeStone(BoardPoint.BLACK, 2, 3);
+    board.placeStone(BoardPoint.BLACK, 4, 5);
+    String expectedResult = "29B27B111";
+    GameBoard newBoard = new GameBoard();
+    newBoard.fromCompactString(expectedResult);
+    assertAll(() -> assertEquals(expectedResult, board.toCompactString()),
+              () -> assertEquals(expectedResult, board.toCompactString()),
+              () -> assertEquals(newBoard, board));
+  }
 
-    @Test
-    void gameCopyIsEqual() {
-        board.placeStone(BoardPoint.BLACK, 0, 0);
-        GameBoard board2 = new GameBoard();
-        board2.setBoard(board);
-        assertEquals(board, board2);
-    }
+  @Test
+  void testToAndFromCompactString() {
+    for (int i = 0; i < board.size(); i++) { board.placeStone(BoardPoint.BLACK, i, 4); }
+    String expectedResult = board.toCompactString();
+    assertEquals("4B12B12B12B12B12B12B12B12B12B12B12B12B8", expectedResult);
+
+    board = new GameBoard();
+    board.fromCompactString(expectedResult);
+    assertEquals("4B12B12B12B12B12B12B12B12B12B12B12B12B8", expectedResult);
+  }
+
+  @Test
+  void gameCopyIsEqual() {
+    board.placeStone(BoardPoint.BLACK, 0, 0);
+    GameBoard board2 = new GameBoard();
+    board2.setBoard(board);
+    assertEquals(board, board2);
+  }
 }
