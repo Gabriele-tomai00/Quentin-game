ddef63ed86e99e562a5a990e4a3a55abd8a9028d
Gabriele <tomaigabriele@gmail.com>
2025-04-08 19:55:52 +0200

    feat: added all the import to the ControllerTest

--- app/src/test/java/quentin/gui/ControllerTest.java
+++ app/src/test/java/quentin/gui/ControllerTest.java
@@ -1,10 +1,19 @@
 package quentin.gui;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.hasText;
 
 @ExtendWith(ApplicationExtension.class)
 class ControllerTest extends ApplicationTest {
