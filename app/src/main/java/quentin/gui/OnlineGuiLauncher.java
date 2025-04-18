26447f3b6fda0888073ea31c37e51b42505bc8fb
alequerenghi <117732128+alequerenghi@users.noreply.github.com>
2025-04-18 14:33:58 +0200

    Update OnlineGuiLauncher.java to initialize the executor

--- app/src/main/java/quentin/gui/OnlineGuiLauncher.java
+++ app/src/main/java/quentin/gui/OnlineGuiLauncher.java
@@ -5,10 +5,15 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.concurrent.Executors;
 
 public class OnlineGuiLauncher extends Application {
     private Parent root;
     private ExecutorService executor;
+
+    public OnlineGuiLauncher() {
+        executor = Executors.newSingleThreadExecutor();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
